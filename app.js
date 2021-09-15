ccf1406d5425548062f5b69827314245aa9abfd1
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-09-15 14:15:31 +0300

    Fixed indentation

--- app.js
+++ app.js
@@ -11,16 +11,22 @@ const mongoose = require('mongoose')
 
 logger.info('Connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-        logger.info('Connected successfully to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('Error while connecting to MongoDB', error.message)
-    })
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => {
+    logger.info('Connected successfully to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error while connecting to MongoDB', error.message)
+  })
 
 app.use(cors())
-// app.use(express.static('build')) Tämä käyttöön sitten kun Reactin buildi on valmis
+//app.use(express.static('./client/build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
@@ -30,14 +36,11 @@ app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 
 if (process.env.NODE_ENV === 'test') {
-    const testingRouter = require('./controllers/testing')
-    app.use('/api/testing', testingRouter)
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
 }
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
-
 module.exports = app
-
-
--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-
 const server = http.createServer(app)
 
 app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
