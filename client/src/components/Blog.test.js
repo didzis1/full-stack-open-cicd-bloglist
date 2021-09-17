8defc3e9f6626108ecf60ee96e6c392904d5f939
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-09-17 10:36:21 +0300

    linted code

--- client/src/components/Blog.test.js
+++ client/src/components/Blog.test.js
@@ -5,61 +5,61 @@ import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('Blog functionality', () => {
-  let component
-  const mockHandler = jest.fn()
+	let component
+	const mockHandler = jest.fn()
 
-  beforeEach(() => {
-    const blog = {
-      title: 'This is ran before each individual test',
-      author: 'Didzis Zvaigzne',
-      url: 'didzis.net',
-      likes: 122,
-      user: {
-        username: 'didzis1'
-      }
-    }
+	beforeEach(() => {
+		const blog = {
+			title: 'This is ran before each individual test',
+			author: 'Didzis Zvaigzne',
+			url: 'didzis.net',
+			likes: 122,
+			user: {
+				username: 'didzis1'
+			}
+		}
 
-    const user = {
-      username: 'didzis1'
-    }
+		const user = {
+			username: 'didzis1'
+		}
 
-    component = render(
-      <Blog blog={blog} user={user} updateBlog={mockHandler} />
-    )
-  })
+		component = render(
+			<Blog blog={blog} user={user} updateBlog={mockHandler} />
+		)
+	})
 
-  test('rendered blog shows title and author', () => {
-    expect(component.container).toHaveTextContent(
-      'This is ran before each individual test'
-    )
+	test('rendered blog shows title and author', () => {
+		expect(component.container).toHaveTextContent(
+			'This is ran before each individual test'
+		)
 
-    expect(component.container).not.toHaveTextContent('facebook.com')
+		expect(component.container).not.toHaveTextContent('facebook.com')
 
-    expect(component.container).not.toHaveTextContent('5000 likes')
+		expect(component.container).not.toHaveTextContent('5000 likes')
 
-    // component.debug()
-  })
+		// component.debug()
+	})
 
-  test('blog renders also url and likes when show button is clicked', () => {
-    // component.debug()
+	test('blog renders also url and likes when show button is clicked', () => {
+		// component.debug()
 
-    const showButton = component.container.querySelector('.showMore')
-    fireEvent.click(showButton)
+		const showButton = component.container.querySelector('.showMore')
+		fireEvent.click(showButton)
 
-    expect(component.container).toHaveTextContent('didzis.net')
+		expect(component.container).toHaveTextContent('didzis.net')
 
-    expect(component.container).toHaveTextContent('122 likes')
-    // component.debug()
-  })
+		expect(component.container).toHaveTextContent('122 likes')
+		// component.debug()
+	})
 
-  test('clicking like button two times, calls event handler twice', () => {
-    const showButton = component.container.querySelector('.showMore')
-    fireEvent.click(showButton)
+	test('clicking like button two times, calls event handler twice', () => {
+		const showButton = component.container.querySelector('.showMore')
+		fireEvent.click(showButton)
 
-    const likeButton = component.container.querySelector('.likeButton')
-    fireEvent.click(likeButton)
-    fireEvent.click(likeButton)
+		const likeButton = component.container.querySelector('.likeButton')
+		fireEvent.click(likeButton)
+		fireEvent.click(likeButton)
 
-    expect(mockHandler.mock.calls).toHaveLength(2)
-  })
+		expect(mockHandler.mock.calls).toHaveLength(2)
+	})
 })
