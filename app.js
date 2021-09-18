47a1c67d823ff4604e309537e0948c8ac65c3be6
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-09-18 12:49:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose
   })
 
 app.use(cors())
-app.use(express.static(__dirname + '/client/build'))
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
